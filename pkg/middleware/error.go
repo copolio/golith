1b63282461a9fb98a97dd79e5197d2bcfac097af
Luke <rlwjd1504@gmail.com>
2023-07-29 23:40:37 +0900

    feat: add error handling middleware for gin

---
+++ pkg/middleware/error.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"time"
+)
+
+type HttpStatus int
+
+var _ error = &ResponseStatusError{}
+
+type ResponseStatusError struct {
+	Timestamp time.Time  `json:"timestamp"`
+	Status    HttpStatus `json:"status"`
+	Meta      any        `json:"meta"`
+	Path      string     `json:"path"`
+	Message   string     `json:"message"`
+}
+
+func (r ResponseStatusError) Error() string {
+	return r.Message
+}
+
+func ErrorHandler(c *gin.Context) {
+	c.Next()
+	for _, err := range c.Errors {
+		log.Default().Printf("Error occurred: %v\n", err)
+	}
+	if len(c.Errors) > 0 {
+		target := ResponseStatusError{}
+		if errors.As(c.Errors[0], &target) {
+			c.JSON(int(target.Status), target)
+		} else {
+			statusError := ResponseStatusError{
+				Timestamp: time.Now(),
+				Status:    http.StatusInternalServerError,
+				Meta:      c.Errors,
+				Path:      c.FullPath(),
+				Message:   c.Errors[0].Error(),
+			}
+			wrappedErr := c.Error(statusError)
+			c.JSON(http.StatusInternalServerError, &wrappedErr)
+		}
+	}
+}
---
+++ pkg/middleware/error_test.go
@@ -0,0 +1,98 @@
+package middleware
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestErrorHandler(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	testContext, _ := gin.CreateTestContext(httptest.NewRecorder())
+	tests := []struct {
+		name string
+		args args
+		err  error
+	}{
+		{
+			name: "givenError_whenResponding_returnResponseStatusError",
+			args: args{
+				c: testContext,
+			},
+			err: errors.New("basic error"),
+		},
+		{
+			name: "givenResponseStatusError_whenResponding_returnResponseStatusError",
+			args: args{
+				c: testContext,
+			},
+			err: ResponseStatusError{
+				Timestamp: time.Now(),
+				Status:    http.StatusBadRequest,
+				Meta:      nil,
+				Path:      "/",
+				Message:   "response status error",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// given
+			// when
+			_ = tt.args.c.Error(tt.err)
+			ErrorHandler(tt.args.c)
+			got := tt.args.c.Errors.Last()
+			// then
+			var want ResponseStatusError
+			if !errors.As(got, &want) {
+				t.Errorf("Error() = %v, err %v", got, want)
+			}
+		})
+	}
+}
+
+func TestResponseStatusError_Error(t *testing.T) {
+	type fields struct {
+		Timestamp time.Time
+		Status    HttpStatus
+		Meta      any
+		Path      string
+		Message   string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "givenResponseStatusError_whenError()_returnErrorMessage",
+			fields: fields{
+				Timestamp: time.Now(),
+				Status:    http.StatusBadRequest,
+				Meta:      nil,
+				Path:      "/",
+				Message:   "I'm test",
+			},
+			want: "I'm test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := ResponseStatusError{
+				Timestamp: tt.fields.Timestamp,
+				Status:    tt.fields.Status,
+				Meta:      tt.fields.Meta,
+				Path:      tt.fields.Path,
+				Message:   tt.fields.Message,
+			}
+			if got := r.Error(); got != tt.want {
+				t.Errorf("Error() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
