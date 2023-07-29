bf7b8255b8069403a8fb8c2ae912770eff226914
Luke <rlwjd1504@gmail.com>
2023-07-29 15:03:31 +0900

    feat: add gorm implementation of CrudRepository

---
+++ pkg/gorm/repository.go
@@ -0,0 +1,32 @@
+package gorm
+
+import (
+	"copolio.com/gobootify/pkg/database"
+	"gorm.io/gorm"
+)
+
+var _ database.CrudRepository[any, any] = &GormRepository[any, any]{}
+
+type GormRepository[T any, K any] struct {
+	tx *gorm.DB
+}
+
+func (g GormRepository[T, K]) WithTransaction(transaction any) {
+	g.tx = transaction.(*gorm.DB)
+}
+
+func (g GormRepository[T, K]) Save(entity T) (*T, error) {
+	result := g.tx.Save(&entity)
+	return &entity, result.Error
+}
+
+func (g GormRepository[T, K]) FindById(id K) (*T, error) {
+	var entity T
+	result := g.tx.First(&entity, id)
+	return &entity, result.Error
+}
+
+func (g GormRepository[T, K]) Delete(entity T) (err error) {
+	result := g.tx.Delete(&entity)
+	return result.Error
+}
