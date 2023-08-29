61c99772e857af22f6a2d78c8ae1717365bb47d1
Luke <rlwjd1504@gmail.com>
2023-08-29 23:34:35 +0900

    feat: add functional CrudRepository

--- pkg/data/repository.go
+++ pkg/data/repository.go
@@ -6,3 +6,9 @@ type CrudRepository[T any, K any] interface {
 	FindById(id K) (*T, error)
 	Delete(entity T) (err error)
 }
+
+type FpCrudRepository[T any, K any] interface {
+	FpSave(entity T) func(arg any) (T, error)
+	FpFindById(id K) func(arg any) (T, error)
+	FpDelete(entity T) func(arg any) error
+}
--- pkg/gorm/repository.go
+++ pkg/gorm/repository.go
@@ -2,35 +2,57 @@ package gorm
 
 import (
 	"github.com/copolio/gin-bootify/pkg/data"
+	"github.com/copolio/gin-bootify/pkg/web"
 	"gorm.io/gorm"
 )
 
-var _ data.CrudRepository[any, any] = &GormRepositoryImpl[any, any]{}
+var _ data.FpCrudRepository[any, any] = &GormRepository[any, any]{}
+var _ data.CrudRepository[any, any] = &GormRepository[any, any]{}
 
-type GormRepository[T any, K any] interface {
-	data.CrudRepository[T, K]
+type GormRepository[T any, K any] struct {
 }
 
-type GormRepositoryImpl[T any, K any] struct {
-	tx *gorm.DB
+func (g GormRepository[T, K]) FpSave(entity T) func(arg any) (T, error) {
+	return func(arg any) (T, error) {
+		tx := arg.(*gorm.DB)
+		result := tx.Save(&entity)
+		return entity, result.Error
+	}
 }
 
-func (g GormRepositoryImpl[T, K]) WithTransaction(transaction any) {
-	g.tx = transaction.(*gorm.DB)
+func (g GormRepository[T, K]) FpFindById(id K) func(arg any) (T, error) {
+	return func(arg any) (T, error) {
+		tx := arg.(*gorm.DB)
+		var entity T
+		result := tx.First(&entity, id)
+		return entity, result.Error
+	}
 }
 
-func (g GormRepositoryImpl[T, K]) Save(entity T) (*T, error) {
-	result := g.tx.Save(&entity)
+func (g GormRepository[T, K]) FpDelete(entity T) func(arg any) error {
+	return func(arg any) error {
+		tx := arg.(*gorm.DB)
+		result := tx.Delete(&entity)
+		return result.Error
+	}
+}
+
+func (g GormRepository[T, K]) WithTransaction(transaction any) {
+	web.GetApplication().GormDB = transaction.(*gorm.DB)
+}
+
+func (g GormRepository[T, K]) Save(entity T) (*T, error) {
+	result := web.GetApplication().GormDB.Save(&entity)
 	return &entity, result.Error
 }
 
-func (g GormRepositoryImpl[T, K]) FindById(id K) (*T, error) {
+func (g GormRepository[T, K]) FindById(id K) (*T, error) {
 	var entity T
-	result := g.tx.First(&entity, id)
+	result := web.GetApplication().GormDB.First(&entity, id)
 	return &entity, result.Error
 }
 
-func (g GormRepositoryImpl[T, K]) Delete(entity T) (err error) {
-	result := g.tx.Delete(&entity)
+func (g GormRepository[T, K]) Delete(entity T) (err error) {
+	result := web.GetApplication().GormDB.Delete(&entity)
 	return result.Error
 }
