b397e58c592dd97383d56aa452f4656e4e54b5de
Luke <rlwjd1504@gmail.com>
2023-09-17 14:41:24 +0900

    fix: keep crudrepository stateless

--- golithdata/repository.go
+++ golithdata/repository.go
@@ -1,14 +1,8 @@
 package golithdata
 
 type CrudRepository[T any, K any] interface {
-	WithTransaction(transaction any)
-	Save(entity T) (*T, error)
-	FindById(id K) (*T, error)
-	Delete(entity T) (err error)
-}
-
-type FpCrudRepository[T any, K any] interface {
-	FpSave(entity T) func(arg any) (T, error)
-	FpFindById(id K) func(arg any) (T, error)
-	FpDelete(entity T) func(arg any) error
+	Save(db any, entity T) (T, error)
+	FindAll(db any, where T) ([]T, error)
+	FindById(db any, id K) (T, error)
+	Delete(db any, entity T) error
 }
--- golithgorm/repository.go
+++ golithgorm/repository.go
@@ -5,54 +5,33 @@ import (
 	"gorm.io/gorm"
 )
 
-var _ golithdata.FpCrudRepository[any, any] = &Repository[any, any]{}
 var _ golithdata.CrudRepository[any, any] = &Repository[any, any]{}
 
 type Repository[T any, K any] struct {
-	db *gorm.DB
 }
 
-func (g Repository[T, K]) FpSave(entity T) func(arg any) (T, error) {
-	return func(arg any) (T, error) {
-		tx := arg.(*gorm.DB)
-		result := tx.Save(&entity)
-		return entity, result.Error
-	}
+func (g Repository[T, K]) Save(db any, entity T) (T, error) {
+	tx := db.(*gorm.DB)
+	result := tx.Save(&entity)
+	return entity, result.Error
 }
 
-func (g Repository[T, K]) FpFindById(id K) func(arg any) (T, error) {
-	return func(arg any) (T, error) {
-		tx := arg.(*gorm.DB)
-		var entity T
-		result := tx.First(&entity, id)
-		return entity, result.Error
-	}
+func (g Repository[T, K]) FindAll(db any, where T) ([]T, error) {
+	tx := db.(*gorm.DB)
+	var entities []T
+	result := tx.Where(&where).Find(&entities)
+	return entities, result.Error
 }
 
-func (g Repository[T, K]) FpDelete(entity T) func(arg any) error {
-	return func(arg any) error {
-		tx := arg.(*gorm.DB)
-		result := tx.Delete(&entity)
-		return result.Error
-	}
-}
-
-func (g Repository[T, K]) WithTransaction(transaction any) {
-	g.db = transaction.(*gorm.DB)
-}
-
-func (g Repository[T, K]) Save(entity T) (*T, error) {
-	result := g.db.Save(&entity)
-	return &entity, result.Error
-}
-
-func (g Repository[T, K]) FindById(id K) (*T, error) {
+func (g Repository[T, K]) FindById(db any, id K) (T, error) {
+	tx := db.(*gorm.DB)
 	var entity T
-	result := g.db.First(&entity, id)
-	return &entity, result.Error
+	result := tx.First(&entity, id)
+	return entity, result.Error
 }
 
-func (g Repository[T, K]) Delete(entity T) (err error) {
-	result := g.db.Delete(&entity)
+func (g Repository[T, K]) Delete(db any, entity T) error {
+	tx := db.(*gorm.DB)
+	result := tx.Delete(&entity)
 	return result.Error
 }
