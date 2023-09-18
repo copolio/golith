e071be672c6931a1302e74b8abf5abce7421d748
Luke <rlwjd1504@gmail.com>
2023-09-18 23:44:22 +0900

    feat: add gin instance constructor for fx

---
+++ golithgin/gin.go
@@ -0,0 +1,28 @@
+package golithgin
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/fx"
+	"net/http"
+)
+
+func NewGin(lc fx.Lifecycle, conf *Configuration) *gin.Engine {
+	gin.SetMode(string(conf.Mode))
+	router := gin.Default()
+	router.Use(HttpErrorHandler())
+
+	server := &http.Server{Addr: fmt.Sprintf(":%d", conf.Port), Handler: router}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			fmt.Println("[golith] Starting Gin HTTP Server at", server.Addr)
+			return server.ListenAndServe()
+		},
+		OnStop: func(ctx context.Context) error {
+			fmt.Println("[golith] Shutting down Gin server")
+			return server.Shutdown(ctx)
+		},
+	})
+	return router
+}
