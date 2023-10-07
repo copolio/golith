a0d0e7d02fb2a018521b57d12c1250b7f981c27a
Luke <rlwjd1504@gmail.com>
2023-10-07 13:47:59 +0900

    chore: make crudrepository gorm specific

--- golithgorm/repository.go
+++ golithgorm/repository.go
@@ -7,37 +7,33 @@ import (
 var _ CrudRepository[any, any] = &Repository[any, any]{}
 
 type CrudRepository[T any, K any] interface {
-	Save(db any, entity T) (T, error)
-	FindAll(db any, where T) ([]T, error)
-	FindById(db any, id K) (T, error)
-	Delete(db any, entity T) error
+	Save(db *gorm.DB, entity T) (T, error)
+	FindAll(db *gorm.DB, where T) ([]T, error)
+	FindById(db *gorm.DB, id K) (T, error)
+	Delete(db *gorm.DB, entity T) error
 }
 
 type Repository[T any, K any] struct {
 }
 
-func (g Repository[T, K]) Save(db any, entity T) (T, error) {
-	tx := db.(*gorm.DB)
-	result := tx.Save(&entity)
+func (g Repository[T, K]) Save(db *gorm.DB, entity T) (T, error) {
+	result := db.Save(&entity)
 	return entity, result.Error
 }
 
-func (g Repository[T, K]) FindAll(db any, where T) ([]T, error) {
-	tx := db.(*gorm.DB)
+func (g Repository[T, K]) FindAll(db *gorm.DB, where T) ([]T, error) {
 	var entities []T
-	result := tx.Where(&where).Find(&entities)
+	result := db.Where(&where).Find(&entities)
 	return entities, result.Error
 }
 
-func (g Repository[T, K]) FindById(db any, id K) (T, error) {
-	tx := db.(*gorm.DB)
+func (g Repository[T, K]) FindById(db *gorm.DB, id K) (T, error) {
 	var entity T
-	result := tx.First(&entity, id)
+	result := db.First(&entity, id)
 	return entity, result.Error
 }
 
-func (g Repository[T, K]) Delete(db any, entity T) error {
-	tx := db.(*gorm.DB)
-	result := tx.Delete(&entity)
+func (g Repository[T, K]) Delete(db *gorm.DB, entity T) error {
+	result := db.Delete(&entity)
 	return result.Error
 }
